5e2ad9344181262e4fd7eab56c03bc47e3078e02
Mariana Souza <marysouzarj@gmail.com>
2023-07-26 09:34:26 -0300

    Create prova.js

---
+++ js/prova.js
@@ -0,0 +1,19 @@
+var temp;
+var A1=12;
+var A2=77;
+var A3 =23;
+
+if(A1>A2){
+    temp = A1;
+    A1 = A2;
+    A2 = temp;
+
+}
+
+if (A2>A3){
+    temp = A2;
+    A2 = A3;
+    A3 = temp;
+}
+
+console.log(A3)
